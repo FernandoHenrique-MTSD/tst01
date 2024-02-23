33aedf1e08d9abe1c153b5b75d96f2e5c2792594
FernandoHenrique-MTSD <158511149+FernandoHenrique-MTSD@users.noreply.github.com>
2024-02-23 11:19:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,5 @@
     <body>
         <?php echo "<p>TESTE 01</p>"; ?>
 <p class="style6">Clique a seguir para visualizar o pedido <a href="https://storagewebappmtsdteste.blob.core.windows.net/web01/pedido-1-5558874.pdf" target="_blank" rel="noopener">PEDIDO</a>.</p>
-clique aqui para exibir o pedido em pdf https://storagewebappmtsdteste.blob.core.windows.net/web01/pedido-1-5558874.pdf
     </body>
 </html>
